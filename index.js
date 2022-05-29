a35738f739c716d6d1462cfc0eeca04900d695ff
thangduc131199 <thang131199@gmail.com>
2022-05-30 06:43:13 +0700

    update connect serial

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const path = require("path");
 const { PORT } = require('./config');
 
+//Serial
+const {SerialPort} = require("serialport");
+
+
 const app = express();
  
 app.use(express.json());
@@ -19,17 +23,68 @@ server = app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}!`);
 });
 
-let startPoint = [103.957908, 22.4978735];
+let startPoint = [105.8200417,20.9772837];
 
 const io = require("socket.io")(server);
 io.on('connection', (socket) => {
     console.log('New user connected');
-    setInterval(() => {
-        let [x, y] = startPoint;
-        x += 0.00001;
-        y += 0.00001;
-        startPoint = [parseFloat(x.toFixed(7)), parseFloat(y.toFixed(7))];
-        console.log(startPoint);
+
+    var port = "/dev/ttyUSB0";
+    var serialPort = new SerialPort({ path: '/dev/ttyUSB0', baudRate: 115200 })
+
+    serialPort.on("open", function() {
+    console.log("-- Connection opened --");
+    serialPort.on("data", function(data_) {
+        // console.log(""+data_.toString())
+        let data=data_.toString();
+        let lat;
+        let lon;
+        // console.log("ahii"+ data);
+        if (data[0]=='s'){
+        let s1=data.indexOf('s');
+        let s2=data.indexOf(',');
+        let s3=data.indexOf('e');
+        // let lat_str = data.substring(s1+1,s2);
+        // console.log(lat_str);
+        lat = parseFloat(data.substring(s1+1,s2));
+        lon = parseFloat(data.substring(s2+1,s3));
+        startPoint = [lon,lat];
+        console.log(lon+ "," + lat);
+        }
+        // let [x, y] = startPoint;
+
+        // startPoint = [lon,lat];
+        // console.log(startPoint);
         io.sockets.emit('new_location', {nextPoint: startPoint});
-    }, 5000);
-});
+        
+    });
+
+        
+    });
+    });
+    // setInterval(() => {
+
+
+    //     let lat,lon;
+    //     serialPort.on("open", function() {
+    //         console.log("-- Connection opened --");
+    //         serialPort.on("data", function(data) {
+    //             console.log(""+data);
+    //             console.log(""+data);
+    //           if (data[0]=='s'&&data[data.lenght]=='e'){
+    //             let s1=data.indexOf('a');
+    //             let s2=data.indexOf(',');
+    //             let s3=data.indexOf('s');
+    //             lat = parseFloat(data.substring(s1+1,s2-1)).toFixed(7);
+    //             lon = parseFloat(data.substring(s2+1,s3-1)).toFixed(7);
+    //           }
+              
+    //         });
+    //       });
+
+    //     let [x, y] = startPoint;
+
+    //     startPoint = [lon,lat];
+    //     console.log(startPoint);
+    //     io.sockets.emit('new_location', {nextPoint: startPoint});
+    // }, 1000);
